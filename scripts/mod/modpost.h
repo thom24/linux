edb3443d773c02ba53eb16fbc6288d91603847c0
Florian Fainelli <f.fainelli@gmail.com>
2017-07-07 17:00:49 +0200

    Add an OSX specific patch to make the kernel be compiled
    lede-commit: 3fc2a24f0422b2f55f9ed43f116db3111f700526
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>

--- scripts/mod/modpost.h
+++ scripts/mod/modpost.h
@@ -9,7 +9,11 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
+#if !(defined(__APPLE__) || defined(__CYGWIN__))
 #include <elf.h>
+#else
+#include "elf.h"
+#endif
 #include "../../include/linux/module_symbol.h"
 
 #include "list.h"

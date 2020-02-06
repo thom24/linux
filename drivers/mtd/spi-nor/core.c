ee8d4181c5d8a981d24cda6934bc9aa69c297841
Daniel Golle <daniel@makrotopia.org>
2020-02-06 19:19:41 +0200

    mtd: spi-nor: Add support for xt25f128b chip
    Add XT25F128B made by XTX Technology (Shenzhen) Limited.
    This chip supports dual and quad read and uniform 4K-byte erase.
    Verified on Teltonika RUT955 which comes with XT25F128B in recent
    versions of the device.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/mtd/spi-nor/core.c
+++ drivers/mtd/spi-nor/core.c
@@ -2017,6 +2017,7 @@ static const struct spi_nor_manufacturer *manufacturers[] = {
 	&spi_nor_winbond,
 	&spi_nor_xilinx,
 	&spi_nor_xmc,
+	&spi_nor_xtx,
 };
 
 static const struct flash_info spi_nor_generic_flash = {

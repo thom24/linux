52505b7870cff1d5aeb15f1b3e29317d65353e6d
Yannick Fertre <yannick.fertre@foss.st.com>
2024-01-23 16:10:58 +0100

    drm/stm: ltdc: set interrupt for CRC
    Enable line interrupt for CRC to return for each frame.
    Add mutex to protect flag for crc & vblank features.
    
    Change-Id: Ie5f68fdc2fdb4609ab114be62985ac71708a7714
    Signed-off-by: Yannick Fertre <yannick.fertre@foss.st.com>

--- drivers/gpu/drm/stm/ltdc.h
+++ drivers/gpu/drm/stm/ltdc.h
@@ -50,6 +50,7 @@ struct ltdc_device {
 	struct clk *pixel_clk;	/* lcd pixel clock */
 	struct clk *bus_clk;	/* bus clock */
 	struct mutex err_lock;	/* protecting error_status */
+	struct mutex act_lock;	/* protecting active status */
 	struct ltdc_caps caps;
 	u32 irq_status;
 	u32 fifo_err;		/* fifo underrun error counter */
@@ -60,6 +61,8 @@ struct ltdc_device {
 	struct drm_atomic_state *suspend_state;
 	int crc_skip_count;
 	bool crc_active;
+	bool vblank_active;
+	u32 crc;
 	u32 max_burst_length;
 };
 

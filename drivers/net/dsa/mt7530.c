c8030dfe624e25fd84a9f28c9e3a7cbfb329aa78
Arınç ÜNAL <arinc.unal@arinc9.com>
2024-04-22 10:15:15 +0300

    net: dsa: mt7530: return mt7530_setup_mdio & mt7531_setup_common on error
    The mt7530_setup_mdio() and mt7531_setup_common() functions should be
    checked for errors. Return if the functions return a non-zero value.
    
    Signed-off-by: Arınç ÜNAL <arinc.unal@arinc9.com>

--- drivers/net/dsa/mt7530.c
+++ drivers/net/dsa/mt7530.c
@@ -2682,7 +2682,9 @@ mt7531_setup(struct dsa_switch *ds)
 					 0);
 	}
 
-	mt7531_setup_common(ds);
+	ret = mt7531_setup_common(ds);
+	if (ret)
+		return ret;
 
 	return 0;
 }
@@ -3027,6 +3029,8 @@ mt753x_setup(struct dsa_switch *ds)
 	ret = mt7530_setup_mdio(priv);
 	if (ret && priv->irq)
 		mt7530_free_irq_common(priv);
+	if (ret)
+		return ret;
 
 	/* Initialise the PCS devices */
 	for (i = 0; i < priv->ds->num_ports; i++) {

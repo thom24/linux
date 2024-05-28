06df012d15d1a3446d25bc4339ab2e529843fb3a
Thomas Richard <thomas.richard@bootlin.com>
2024-05-28 14:41:06 +0200

    mfd: add Congatec Board Controller mfd driver
    Add core MFD driver for the Board Controller found on some Congatec SMARC
    module. This Board Controller provides functions like watchdog, GPIO, and
    I2C busses.
    
    This commit add support only for the conga-SA7 module.
    
    Signed-off-by: Thomas Richard <thomas.richard@bootlin.com>

---
+++ include/linux/mfd/cgbc.h
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Congatec Board Controller driver definitions
+ *
+ * Copyright (C) 2024 Bootlin
+ * Author: Thomas Richard <thomas.richard@bootlin.com>
+ */
+
+#define CGBC_CGBC_CMD_GET_FW_REV	0x21
+
+#define CGBC_VERSION_LEN 10
+
+struct cgbc_info {
+	unsigned char feature;
+	unsigned char major;
+	unsigned char minor;
+	char version[CGBC_VERSION_LEN];
+};
+
+struct cgbc_device_data {
+	void __iomem		*io_session;
+	void __iomem		*io_cmd;
+	u8			session;
+	struct device		*dev;
+	struct cgbc_info	info;
+	struct mutex		lock;
+};
+
+struct cgbc_platform_data {
+	const struct resource	*ioresource;
+	unsigned int num_ioresource;
+	int	(*command)(struct cgbc_device_data *cgbc,
+			   u8 *cmd, u8 cmd_size,
+			   u8 *data, u8 data_size,
+			   u8 *status);
+	int	(*register_cells)(struct cgbc_device_data *cgbc);
+	int	(*map)(struct cgbc_device_data *cgbc);
+	int	(*init)(struct cgbc_device_data *cgbc);
+	void	(*close)(struct cgbc_device_data *cgbc);
+};
+
+int cgbc_command(struct cgbc_device_data *cgbc, void *cmd, unsigned int cmd_size,
+		 void *data, unsigned int data_size, u8 *status);

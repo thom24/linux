a0826475a1db71ddb526a455edf9807062d3209d
Thomas Richard <thomas.richard@bootlin.com>
2024-06-19 20:29:48 +0200

    Use struct cgbc_i2c_data
    Signed-off-by: Thomas Richard <thomas.richard@bootlin.com>

--- drivers/i2c/busses/i2c-cgbc.c
+++ drivers/i2c/busses/i2c-cgbc.c
@@ -49,7 +49,13 @@ enum {
 };
 
 struct i2c_algo_cgbc_data {
-	int			bus_id;
+	u8	bus_id;
+};
+
+struct cgbc_i2c_data {
+	struct device		*dev;
+	struct cgbc_device_data *cgbc;
+	struct i2c_adapter      adap;
 	struct i2c_msg		*msg;
 	int			nmsgs;
 	int			pos;
@@ -72,8 +78,9 @@ MODULE_PARM_DESC(bus_frequency, "Set I2C bus frequency in kHz (default="
 
 static int cgbc_i2c_get_status(struct i2c_adapter *adap)
 {
-	struct cgbc_device_data *cgbc = i2c_get_adapdata(adap);
 	struct i2c_algo_cgbc_data *algo_data = adap->algo_data;
+	struct cgbc_i2c_data *i2c = i2c_get_adapdata(adap);
+	struct cgbc_device_data *cgbc = i2c->cgbc;
 	u8 cmd = CGBC_I2C_CMD_STAT | algo_data->bus_id;
 	u8 status;
 	int ret;
@@ -87,8 +94,9 @@ static int cgbc_i2c_get_status(struct i2c_adapter *adap)
 
 static int cgbc_i2c_set_frequency(struct i2c_adapter *adap, unsigned int bus_frequency)
 {
-	struct cgbc_device_data *cgbc = i2c_get_adapdata(adap);
 	struct i2c_algo_cgbc_data *algo_data = adap->algo_data;
+	struct cgbc_i2c_data *i2c = i2c_get_adapdata(adap);
+	struct cgbc_device_data *cgbc = i2c->cgbc;
 	u8 cmd[2], data;
 	int ret;
 
@@ -104,21 +112,22 @@ static int cgbc_i2c_set_frequency(struct i2c_adapter *adap, unsigned int bus_fre
 	else
 		cmd[1] = CGBC_I2C_FREQ_UNIT_1KHZ | bus_frequency;
 
-	ret = cgbc_command(cgbc, &cmd[0], sizeof(cmd), &data, 1, NULL);
+	ret = cgbc_command(cgbc, &cmd, sizeof(cmd), &data, 1, NULL);
 	if (ret)
-		return dev_err_probe(&adap->dev, ret, "Failed to initialize I2C bus %s",
+		return dev_err_probe(i2c->dev, ret, "Failed to initialize I2C bus %s",
 				     adap->name);
 
-	dev_info(&adap->dev, "%s initialized at %dkHz\n", adap->name, 100);
+	dev_info(i2c->dev, "%s initialized at %dkHz\n", adap->name, 100);
 
 	return 0;
 }
 
 static int cgbc_i2c_xfer_msg(struct i2c_adapter *adap)
 {
-	struct cgbc_device_data *cgbc = i2c_get_adapdata(adap);
 	struct i2c_algo_cgbc_data *algo_data = adap->algo_data;
-	struct i2c_msg *msg = algo_data->msg;
+	struct cgbc_i2c_data *i2c = i2c_get_adapdata(adap);
+	struct cgbc_device_data *cgbc = i2c->cgbc;
+	struct i2c_msg *msg = i2c->msg;
 	int ret, max_len, len, i;
 	u8 cmd_data;
 
@@ -127,7 +136,7 @@ static int cgbc_i2c_xfer_msg(struct i2c_adapter *adap)
 		.addr = i2c_8bit_addr_from_msg(msg),
 	};
 
-	if (algo_data->state == STATE_DONE)
+	if (i2c->state == STATE_DONE)
 		return ret;
 
 	ret = cgbc_i2c_get_status(adap);
@@ -137,36 +146,36 @@ static int cgbc_i2c_xfer_msg(struct i2c_adapter *adap)
 	else if (ret < 0)
 		goto err;
 
-	if (algo_data->state == STATE_INIT || algo_data->state == STATE_WRITE) {
+	if (i2c->state == STATE_INIT || i2c->state == STATE_WRITE) {
 		xfer.write = CGBC_I2C_START;
-		algo_data->state = (msg->flags & I2C_M_RD) ? STATE_READ : STATE_WRITE;
+		i2c->state = (msg->flags & I2C_M_RD) ? STATE_READ : STATE_WRITE;
 	} else
 		xfer.write = 0x00;
 
-	max_len = (algo_data->state == STATE_READ) ? CGBC_I2C_READ_MAX_LEN : CGBC_I2C_WRITE_MAX_LEN;
-	if (msg->len - algo_data->pos > max_len)
+	max_len = (i2c->state == STATE_READ) ? CGBC_I2C_READ_MAX_LEN : CGBC_I2C_WRITE_MAX_LEN;
+	if (msg->len - i2c->pos > max_len)
 		len = max_len;
 	else {
-		len = msg->len - algo_data->pos;
+		len = msg->len - i2c->pos;
 
-		if (algo_data->nmsgs == 1)
+		if (i2c->nmsgs == 1)
 			xfer.write |= CGBC_I2C_STOP;
 	}
 
-	if (algo_data->state == STATE_WRITE) {
+	if (i2c->state == STATE_WRITE) {
 		xfer.write |= (1 + len);
 		xfer.read = 0x00;
 		for (i = 0; i < len; i++)
-			xfer.data[i] = msg->buf[algo_data->pos + i];
+			xfer.data[i] = msg->buf[i2c->pos + i];
 
 		ret =  cgbc_command(cgbc, &xfer, CGBC_I2C_TRANSFER_HEADER_SIZE + len, NULL, 0, NULL);
 		if (ret)
 			goto err;
-	} else if (algo_data->state == STATE_READ) {
+	} else if (i2c->state == STATE_READ) {
 		xfer.write |= 1;
 
 		xfer.read = len;
-		if (algo_data->nmsgs > 1 || msg->len - algo_data->pos > max_len)
+		if (i2c->nmsgs > 1 || msg->len - i2c->pos > max_len)
 			xfer.read |= CGBC_I2C_LAST_ACK;
 
 		ret = cgbc_command(cgbc, &xfer, CGBC_I2C_TRANSFER_HEADER_SIZE, NULL, 0, NULL);
@@ -179,53 +188,53 @@ static int cgbc_i2c_xfer_msg(struct i2c_adapter *adap)
 			goto err;
 
 		cmd_data = CGBC_I2C_CMD_DATA | algo_data->bus_id;
-		ret = cgbc_command(cgbc, &cmd_data, sizeof(cmd_data), msg->buf + algo_data->pos, len, NULL);
+		ret = cgbc_command(cgbc, &cmd_data, sizeof(cmd_data), msg->buf + i2c->pos, len, NULL);
 		if (ret)
 			goto err;
 	}
 
-	if (!ret && (algo_data->state == STATE_WRITE || algo_data->state == STATE_READ)) {
-		if (len == (msg->len - algo_data->pos)) {
-			algo_data->msg++;
-			algo_data->nmsgs--;
-			algo_data->pos = 0;
+	if (!ret && (i2c->state == STATE_WRITE || i2c->state == STATE_READ)) {
+		if (len == (msg->len - i2c->pos)) {
+			i2c->msg++;
+			i2c->nmsgs--;
+			i2c->pos = 0;
 		} else
-			algo_data->pos += len;
+			i2c->pos += len;
 	}
 
-	if (algo_data->nmsgs == 0)
-		algo_data->state = STATE_DONE;
+	if (i2c->nmsgs == 0)
+		i2c->state = STATE_DONE;
 
 	return 0;
 
 err:
-	algo_data->state = STATE_ERROR;
+	i2c->state = STATE_ERROR;
 	return ret;
 }
 
 
 static int cgbc_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg *msgs, int num)
 {
-	struct i2c_algo_cgbc_data *algo_data = adap->algo_data;
+	struct cgbc_i2c_data *i2c = i2c_get_adapdata(adap);
 	unsigned long timeout = jiffies + HZ;
 	int ret;
 
-	algo_data->state = STATE_INIT;
-	algo_data->msg = msgs;
-	algo_data->nmsgs = num;
-	algo_data->pos = 0;
+	i2c->state = STATE_INIT;
+	i2c->msg = msgs;
+	i2c->nmsgs = num;
+	i2c->pos = 0;
 
 	while (time_before(jiffies, timeout)) {
 		ret = cgbc_i2c_xfer_msg(adap);
 
-		if (algo_data->state == STATE_DONE || algo_data->state == STATE_ERROR)
-			return (algo_data->state == STATE_DONE) ? num : ret;
+		if (i2c->state == STATE_DONE || i2c->state == STATE_ERROR)
+			return (i2c->state == STATE_DONE) ? num : ret;
 
 		if (ret == 0)
 			timeout = jiffies + HZ;
 	}
 
-	algo_data->state = STATE_ERROR;
+	i2c->state = STATE_ERROR;
 
 	return -ETIMEDOUT;
 }
@@ -240,12 +249,12 @@ static const struct i2c_algorithm cgbc_i2c_algorithm = {
 	.functionality	= cgbc_i2c_func,
 };
 
-static struct i2c_algo_cgbc_data cgbc_i2c_algo_data[2] = {
+static struct i2c_algo_cgbc_data cgbc_i2c_algo_data[] = {
 	{ .bus_id = CGBC_I2C_PRIMARY_BUS_ID },
 	{ .bus_id = CGBC_I2C_PM_BUS_ID },
 };
 
-static struct i2c_adapter cgbc_i2c_adapter[2] = {
+static const struct i2c_adapter cgbc_i2c_adapter[] = {
 	{
 		.owner		= THIS_MODULE,
 		.name		= "Congatec Primary I2C adapter",
@@ -256,7 +265,7 @@ static struct i2c_adapter cgbc_i2c_adapter[2] = {
 	},
 	{
 		.owner		= THIS_MODULE,
-		.name		= "Congatec Power Mangement I2C adapter",
+		.name		= "Congatec Power Management I2C adapter",
 		.class		= I2C_CLASS_DEPRECATED,
 		.algo		= &cgbc_i2c_algorithm,
 		.algo_data	= &cgbc_i2c_algo_data[1],
@@ -266,25 +275,33 @@ static struct i2c_adapter cgbc_i2c_adapter[2] = {
 
 static int cgbc_i2c_probe(struct platform_device *pdev)
 {
-	struct i2c_adapter *adap = &cgbc_i2c_adapter[pdev->id];
-	struct device *dev = &pdev->dev;
-	struct cgbc_device_data *cgbc = dev_get_drvdata(dev->parent);
+	struct cgbc_device_data *cgbc = dev_get_drvdata(pdev->dev.parent);
+	struct cgbc_i2c_data *i2c;
 	int ret;
 
-	adap->dev.parent = dev;
+	i2c = devm_kzalloc(&pdev->dev, sizeof(*i2c), GFP_KERNEL);
+	if (!i2c)
+		return -ENOMEM;
 
-	i2c_set_adapdata(adap, cgbc);
+	i2c->cgbc = cgbc;
+	i2c->dev = &pdev->dev;
+	i2c->adap = cgbc_i2c_adapter[pdev->id];
+	i2c->adap.dev.parent = i2c->dev;
+	i2c_set_adapdata(&i2c->adap, i2c);
+	platform_set_drvdata(pdev, i2c);
 
-	ret = cgbc_i2c_set_frequency(adap, bus_frequency);
+	ret = cgbc_i2c_set_frequency(&i2c->adap, bus_frequency);
 	if (ret)
 		return ret;
 
-	return i2c_add_numbered_adapter(adap);
+	return i2c_add_numbered_adapter(&i2c->adap);
 }
 
 static void cgbc_i2c_remove(struct platform_device *pdev)
 {
-	i2c_del_adapter(&cgbc_i2c_adapter[pdev->id]);
+	 struct cgbc_i2c_data *i2c = platform_get_drvdata(pdev);
+
+	i2c_del_adapter(&i2c->adap);
 }
 
 static struct platform_driver cgbc_i2c_driver = {

5012c936f3dad4b6c0dd313d58446735ce586b2b
Christophe Roullier <christophe.roullier@foss.st.com>
2024-09-04 09:04:38 +0200

    Revert "ptp: Add .getmaxphase callback to ptp_clock_info"
    This reverts commit c3b60ab7a4dff6e6e608e685b70ddc3d6b2aca81.
    
    Change-Id: Ie9301222fcf0f595769367dcd895f037f07a68cd
    Signed-off-by: Christophe Roullier <christophe.roullier@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/404470

--- tools/testing/selftests/ptp/testptp.c
+++ tools/testing/selftests/ptp/testptp.c
@@ -337,8 +337,7 @@ int main(int argc, char *argv[])
 			       "  %d pulse per second\n"
 			       "  %d programmable pins\n"
 			       "  %d cross timestamping\n"
-			       "  %d adjust_phase\n"
-			       "  %d maximum phase adjustment (ns)\n",
+			       "  %d adjust_phase\n",
 			       caps.max_adj,
 			       caps.n_alarm,
 			       caps.n_ext_ts,
@@ -346,8 +345,7 @@ int main(int argc, char *argv[])
 			       caps.pps,
 			       caps.n_pins,
 			       caps.cross_timestamping,
-			       caps.adjust_phase,
-			       caps.max_phase_adj);
+			       caps.adjust_phase);
 		}
 	}
 

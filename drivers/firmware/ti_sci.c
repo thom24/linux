b0ce5784782c54deaa25d56633999d71158933b4
Thomas Richard <thomas.richard@bootlin.com>
2025-10-16 16:57:52 +0200

    firmware: ti_sci: skip ti_sci_msg_cmd_lpm_wake_reason
    Signed-off-by: Thomas Richard <thomas.richard@bootlin.com>

--- drivers/firmware/ti_sci.c
+++ drivers/firmware/ti_sci.c
@@ -1861,6 +1861,8 @@ static int ti_sci_msg_cmd_lpm_wake_reason(const struct ti_sci_handle *handle,
 	struct device *dev;
 	int ret = 0;
 
+	return 0;
+
 	if (IS_ERR(handle))
 		return PTR_ERR(handle);
 	if (!handle)

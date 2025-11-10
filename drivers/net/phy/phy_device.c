a32d8b191f4cb09d3032c7b7ef1e7be27ac8e8a4
Gabor Juhos <juhosg@openwrt.org>
2025-11-10 18:14:28 +0100

    generic: add detach callback to struct phy_driver
    lede-commit: fe61fc2d7d0b3fb348b502f68f98243b3ddf5867
    
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -1912,6 +1912,9 @@ void phy_detach(struct phy_device *phydev)
 		phydev->devlink = NULL;
 	}
 
+	if (phydev->drv && phydev->drv->detach)
+		phydev->drv->detach(phydev);
+
 	if (phydev->sysfs_links) {
 		if (dev)
 			sysfs_remove_link(&dev->dev.kobj, "phydev");

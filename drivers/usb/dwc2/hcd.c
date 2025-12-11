afe58aacb1b99008a6eb04686d8196622d0a585a
Fabrice Gasnier <fabrice.gasnier@foss.st.com>
2025-12-11 16:49:11 +0100

    usb: dwc2: add runtime PM routines for debugfs
    Debug file system (regdump) requires the core to exit power saving,
    before the registers can be dumped. Not doing so lead to platform
    freeze. Debugfs regset32 optionally manages runtime PM get_sync/put
    calls when the device struct is provided.
    
    So add runtime suspend and resume routines to dwc2 debugfs driver.
    A specific device is allocated, to manage the debugfs read (resume),
    requests separately, not to disturb the bus suspended state, as
    commented in dwc2_runtime_suspend() routine. The real power saving
    entry is managed from the platform dwc2_runtime_suspend() routine,
    to ensure all parts are actually suspended (gadget, host, debugfs).
    
    Provide device struct to regset32. Take care to manage concurrency with
    bus suspend states by checking check device suspend state.
    The power saving routines are widely called from IRQ subroutines, so
    take care to manage the critical sections.
    
    Signed-off-by: Fabrice Gasnier <fabrice.gasnier@foss.st.com>
    Change-Id: I92fef765058150f052505a7c99506ac6502321ff
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/515564
    Reviewed-by: Christian BRUEL <christian.bruel@foss.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- drivers/usb/dwc2/hcd.c
+++ drivers/usb/dwc2/hcd.c
@@ -3318,7 +3318,8 @@ int dwc2_port_suspend(struct dwc2_hsotg *hsotg, u16 windex)
 
 	if (!hsotg->rpm_suspended) {
 		spin_unlock_irqrestore(&hsotg->lock, flags);
-		dwc2_host_enter_lp(hsotg);
+		hsotg->rpm_suspended = true;
+		pm_runtime_put_sync(hsotg->dev);
 		spin_lock_irqsave(&hsotg->lock, flags);
 	}
 
@@ -4359,7 +4360,8 @@ static int _dwc2_hcd_suspend(struct usb_hcd *hcd)
 
 	if (!hsotg->rpm_suspended) {
 		spin_unlock_irqrestore(&hsotg->lock, flags);
-		dwc2_host_enter_lp(hsotg);
+		hsotg->rpm_suspended = true;
+		pm_runtime_put_sync(hsotg->dev);
 		spin_lock_irqsave(&hsotg->lock, flags);
 
 		/* After entering suspend, hardware is not accessible */
@@ -5773,8 +5775,6 @@ static int dwc2_host_enter_partial_power_down(struct dwc2_hsotg *hsotg)
 	hsotg->lx_state = DWC2_L2;
 	hsotg->bus_suspended = true;
 
-	pm_runtime_put(hsotg->dev);
-	hsotg->rpm_suspended = true;
 	dev_dbg(hsotg->dev, "Entering host partial power down completed.\n");
 
 	return ret;
@@ -5919,9 +5919,6 @@ static void dwc2_host_enter_clock_gating(struct dwc2_hsotg *hsotg)
 
 	hsotg->bus_suspended = true;
 	hsotg->lx_state = DWC2_L2;
-
-	pm_runtime_put(hsotg->dev);
-	hsotg->rpm_suspended = true;
 }
 
 /**

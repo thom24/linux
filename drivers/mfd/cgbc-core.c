c7ab5788604f2fe8cf9e9866755d133415298552
Thomas Richard (congatec GmbH) <thomas.richard@bootlin.com>
2026-07-17 12:14:00 +0200

    mfd: cgbc: Add virtual I2C bus support
    Internally the Board Controller has a virtual I2C bus with some virtual
    storage devices (EEPROM and RAM). So add a new I2C cell for this I2C
    virtual bus.
    
    Signed-off-by: Thomas Richard (congatec GmbH) <thomas.richard@bootlin.com>

--- drivers/mfd/cgbc-core.c
+++ drivers/mfd/cgbc-core.c
@@ -66,6 +66,12 @@ static const struct cgbc_i2c_platform_data cgbc_i2c_pm_pdata = {
 	.cgbc_bus_id = 4,
 };
 
+static const struct cgbc_i2c_platform_data cgbc_i2c_virtual_pdata = {
+	.name  = "Congatec Virtual I2C adapter",
+	.cgbc_bus_id = 3,
+	.fixed_freq = true,
+};
+
 static const struct mfd_cell cgbc_devs[] = {
 	{ .name = "cgbc-backlight" },
 	{ .name = "cgbc-gpio" },
@@ -79,6 +85,11 @@ static const struct mfd_cell cgbc_devs[] = {
 		.name = "cgbc-i2c",
 		.platform_data = &cgbc_i2c_pm_pdata,
 		.pdata_size = sizeof(cgbc_i2c_pm_pdata),
+	},
+	{
+		.name = "cgbc-i2c",
+		.platform_data = &cgbc_i2c_virtual_pdata,
+		.pdata_size = sizeof(cgbc_i2c_virtual_pdata),
 	},
 	{ .name = "cgbc-wdt" },
 };

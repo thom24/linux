b1afd1e9a905b3a16e91e4e0dd1498a884eac9e7
Thomas Richard (congatec GmbH) <thomas.richard@bootlin.com>
2026-08-13 14:14:02 +0200

    mfd: cgbc: Add I2C platform data
    Define I2C platform data for each I2C bus in the cgbc MFD driver. Platform
    data passes per-bus parameters to the I2C driver, rather than being defined
    inside i2c-cgbc itself. This makes the i2c-cgbc driver more generic.
    
    Signed-off-by: Thomas Richard (congatec GmbH) <thomas.richard@bootlin.com>

---
+++ include/linux/platform_data/i2c-cgbc.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * i2c-cgbc interface to platform code
+ *
+ * Copyright (C) 2026 congatec GmbH
+ * Author: Thomas Richard <thomas.richard@bootlin.com>
+ */
+
+#ifndef _LINUX_I2C_CGBC_H
+#define _LINUX_I2C_CGBC_H
+
+/**
+ * struct cgbc_platform_data - Platform data of the CGBC I2C driver
+ * @name:		I2C adapter name
+ * @cgbc_bus_id:	I2C bus ID (from Board Controller point of view)
+ */
+struct cgbc_i2c_platform_data {
+	const char *name;
+	int cgbc_bus_id;
+};
+
+#endif /* _LINUX_I2C_CGBC_H */
